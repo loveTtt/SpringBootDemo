34d2a667f19d31ce8b3d7d0796e5ba6a83a2841d
loveT <a1739344326@163.com>
2023-06-11 21:29:35 +0800

    引入redis

---
+++ src/main/java/com/dream/demo/entity/StorageType.java
@@ -0,0 +1,12 @@
+package com.dream.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class StorageType {
+    private String id;
+
+    private String name;
+
+    private String path;
+}
--- src/test/java/com/dream/demo/user/Test.java
+++ src/test/java/com/dream/demo/user/Test.java
@@ -1,6 +1,5 @@
 package com.dream.demo.user;
 
-
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.ZipUtil;
 import cn.hutool.extra.compress.CompressUtil;
@@ -9,6 +8,21 @@ import reactor.test.StepVerifier;
 
 import java.io.File;
 import java.time.Duration;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import com.dream.demo.entity.Company;
+import com.google.common.util.concurrent.AtomicDouble;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
 /**
  * @author huangys2
@@ -26,6 +40,5 @@ public class Test {
         File srcFile = new File("/path/to/rar/file.rar");
         File destDir = new File("/path/to/destination/dir");
     }
-
 
 }
